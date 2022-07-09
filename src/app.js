af1810084b52833d2159c75a3ed774b8ef44a850
Genadi Tsolov <genadi.tsolov@gmail.com>
2022-07-09 17:10:17 +0300

    added some styles

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ const main = async () => {
   const PORT = 5000;
 
   app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
+  app.use(express.urlencoded({ extended: true }));
 
   app.set("view engine", "ejs");
   app.set("views", path.join(__dirname, "views"));
--- src/routes/index.js
+++ src/routes/index.js
@@ -17,25 +17,22 @@ AuthRouter.post("/register", async (req, res) => {
     // console.log(firstName, lastName, email, password);
     const userId = await saveUser(firstName, lastName, email, password);
 
-    if (userId === null)
-      res
-        .status(400)
-        .render("message", {
-          title: "Error!",
-          message: "Registration Failed!",
-        });
-
-    await sendEmail(
-      email,
-      "Verify Email Address",
-      `<p>To verify your email address click <a href="http://localhost:5000/verify-email/${userId}">here</a>!</p>`
-    );
-    res
-      .status(200)
-      .render("message", {
+    if (userId === null) {
+      res.status(400).render("message", {
+        title: "Error!",
+        message: "Registration Failed!",
+      });
+    } else {
+      await sendEmail(
+        email,
+        "Verify Email Address",
+        `<p>To verify your email address click <a href="http://localhost:5000/verify-email/${userId}">here</a>!</p>`
+      );
+      res.status(200).render("message", {
         title: "Success!",
         message: "Registration was Successful!",
       });
+    }
   } catch (e) {
     res.redirect("/register");
   }
@@ -45,12 +42,10 @@ AuthRouter.get("/verify-email/:id", async (req, res) => {
   const { id } = req.params;
   const verified = await verifyUser(id);
   if (verified) {
-    res
-      .status(200)
-      .render("message", {
-        title: "Success!",
-        message: "Successfully Verified!",
-      });
+    res.status(200).render("message", {
+      title: "Success!",
+      message: "Successfully Verified!",
+    });
   } else {
     res
       .status(400)
